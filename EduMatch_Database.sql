141c89c56162e6b14f9008b4ede6f357b24eebc1
HungNgo03 <hungnnhe173202@fpt.edu.vn>
2025-02-13 01:51:17 +0700

    Add some folder

---
+++ EduMatch_Database.sql
@@ -0,0 +1,75 @@
+﻿CREATE DATABASE TutorFinder;
+GO
+
+USE TutorFinder;
+GO
+CREATE TABLE Users (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Username NVARCHAR(50) UNIQUE NOT NULL,
+    PasswordHash NVARCHAR(255) NOT NULL,
+    FullName NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(100) UNIQUE NOT NULL,
+    PhoneNumber NVARCHAR(15) UNIQUE NOT NULL,
+    Role NVARCHAR(10) CHECK (Role IN ('Student', 'Tutor', 'Admin','System Admin')) NOT NULL,
+    CreatedAt DATETIME DEFAULT GETDATE()
+);
+CREATE TABLE Tutors (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    UserID INT UNIQUE NOT NULL,
+    Gender BIT NOT NULL, -- 0: Male, 1: Female
+    DateOfBirth DATE NOT NULL,
+    Address NVARCHAR(255) NOT NULL,
+    Qualification NVARCHAR(255) NOT NULL,
+    Experience INT CHECK (Experience >= 0),
+    Bio NVARCHAR(MAX),
+	Status int,
+    FOREIGN KEY (UserID) REFERENCES Users(ID) ON DELETE CASCADE
+);
+CREATE TABLE Students (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    UserID INT UNIQUE NOT NULL,
+    ParentName NVARCHAR(100),
+    Grade NVARCHAR(20),
+    Address NVARCHAR(255),
+    Notes NVARCHAR(MAX),
+    FOREIGN KEY (UserID) REFERENCES Users(ID) ON DELETE CASCADE
+);
+CREATE TABLE Subjects (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    SubjectName NVARCHAR(100) UNIQUE NOT NULL,
+    Description NVARCHAR(MAX)
+);
+CREATE TABLE TutorSubjects (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    TutorID INT NOT NULL,
+    SubjectID INT NOT NULL,
+    FOREIGN KEY (TutorID) REFERENCES Tutors(ID) ON DELETE CASCADE,
+    FOREIGN KEY (SubjectID) REFERENCES Subjects(ID) ON DELETE CASCADE,
+    UNIQUE (TutorID, SubjectID) -- Đảm bảo một gia sư không đăng ký trùng môn
+);
+CREATE TABLE Classes (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    TutorID INT NOT NULL,
+    StudentID INT NOT NULL,
+    SubjectID INT NOT NULL,
+    StartDate DATE NOT NULL,
+    EndDate DATE,
+    Status NVARCHAR(20) CHECK (Status IN ('Pending', 'Ongoing', 'Completed')) NOT NULL DEFAULT 'Pending',
+
+    FOREIGN KEY (TutorID) REFERENCES Tutors(ID) ON DELETE CASCADE,
+    FOREIGN KEY (StudentID) REFERENCES Students(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (SubjectID) REFERENCES Subjects(ID) ON DELETE NO ACTION
+);
+
+
+
+CREATE TABLE Schedules (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    ClassID INT NOT NULL,
+    Date DATE NOT NULL,
+    StartTime TIME NOT NULL,
+    EndTime TIME NOT NULL,
+    FOREIGN KEY (ClassID) REFERENCES Classes(ID) ON DELETE CASCADE
+);
+
+
