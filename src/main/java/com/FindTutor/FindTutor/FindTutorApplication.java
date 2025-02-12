31c43582a12b39caf27e5fa79d0e53715fb7ff32
HungNgo03 <hungnnhe173202@fpt.edu.vn>
2025-02-12 21:37:35 +0700

    first commit

---
+++ src/main/java/com/FindTutor/FindTutor/FindTutorApplication.java
@@ -0,0 +1,13 @@
+package com.FindTutor.FindTutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindTutorApplication.class, args);
+	}
+
+}
