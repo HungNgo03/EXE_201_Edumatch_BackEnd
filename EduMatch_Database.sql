80d799f5169f7ef3c8be28ddaba865920158229c
HungNgo03 <hungnnhe173202@fpt.edu.vn>
2025-02-25 23:10:01 +0700

    Modify Database

--- EduMatch_Database.sql
+++ EduMatch_Database.sql
@@ -25,7 +25,8 @@ CREATE TABLE Tutors (
                         Bio NVARCHAR(MAX),
                         Status int,
                         bank_image NVARCHAR(100),
-                        FOREIGN KEY (UserID) REFERENCES Users(ID) ON DELETE CASCADE
+                        money_per_slot money,
+                            FOREIGN KEY (UserID) REFERENCES Users(ID) ON DELETE CASCADE
 );
 CREATE TABLE Students (
                           ID INT IDENTITY(1,1) PRIMARY KEY,
@@ -137,3 +138,64 @@ CREATE TABLE ClassRegistrations (
                                     FOREIGN KEY (ClassID) REFERENCES Classes(ID) ON DELETE NO ACTION,
                                     UNIQUE (StudentID, ClassID)
 );
+INSERT INTO Users (username, password_hash, fullname, Email, phone_number, Role, image)
+VALUES
+    ('huongtt0410', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Trần Thiên Hương', 'huongtt04102002@gmail.com', '111111111', 'Tutor', NULL),
+    ('linhdan_official', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Bùi Thị Linh Đan', 'linhdan.official@gmail.com', '111111112', 'Tutor', NULL),
+    ('nguyenmai2607', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Nguyễn Thị Mai', 'nguyenmaihn2607@gmail.com', '111111113', 'Tutor', NULL),
+    ('nguyenquechi18103', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Nguyễn Quế Chi', 'nguyenquechi18103@gmail.com', '111111114', 'Tutor', NULL),
+    ('minhquantspt1', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Hà Minh Quân', 'minhquantspt1@gmail.com', '111111115', 'Tutor', NULL),
+    ('thuylaw1906', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Vương Thị Thanh Thủy', 'thuylaw1906@gmail.com', '111111116', 'Tutor', NULL),
+    ('tamntqnguyen03', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Nguyễn Thị Quyết Tâm', 'tamntqnguyen03@gmail.com', '111111117', 'Tutor', NULL),
+    ('ngocanh2203', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Trương Ngọc Ánh', 'ngocanh22031999@gmail.com', '111111118', 'Tutor', NULL),
+    ('khoidnwork', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Đinh Ngọc Khôi', 'khoidnwork@gmail.com', '111111119', 'Tutor', NULL),
+    ('quanghuy2003', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Bùi Quang Huy', 'quanghuy2003@gmail.com', '0345025423', 'Tutor', NULL),
+    ('dattiendinh2003', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Đinh Tiến Đạt', 'dattiendinh2003@gmail.com', '111111121', 'Tutor', NULL),
+    ('ducsamuelng', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Nguyễn Thái Đức', 'ducsamuelng@gmail.com', '111111131', 'Tutor', NULL),
+    ('tien_dung2003', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Đỗ Tiến Dũng', 'tiendung@gmail.com', '111111141', 'Tutor', NULL),
+    ('trangttths176143', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Trần Thị Thanh Trang', 'trangttths176143@fpt.edu.vn', '111111151', 'Tutor', NULL),
+    ('phamngoch2003', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Phạm Ngọc Hà', 'ngocha@gmail.com', '111111161', 'Tutor', NULL),
+    ('trangmun2703', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Dương Quỳnh Trang', 'trangmun2703@gmail.com', '111111171', 'Tutor', NULL),
+    ('quanh12102003', '$2a$10$kPMxBz56.g/Gd8/PbmF0S.MqvE7jjmiRygh3rRTtTRGT3fTjEFZWK', N'Đặng Quỳnh Anh', 'quanhquanh12102003@gmail.com', '111111181', 'Tutor', NULL);
+
+INSERT INTO Tutors (UserID, Gender, DateOfBirth, Address, Qualification, Experience, Bio, Status, bank_image)
+VALUES
+    (1, 1, '2002-10-04', N'Hà Nội', N'Đại học', 4, N'Kinh nghiệm gia sư 4 năm môn toán cấp 2,3', 1, NULL),
+    (2, 1, '2003-05-25', N'Hà Nội', N'Đại học', 3, N'Kinh nghiệm gia sư 3 năm môn toán, lý cấp 3, ôn thi THPTQG', 1, NULL),
+    (3, 1, '2003-07-26', N'Hà Nội', N'Đại học', 4, N'Kinh nghiệm gia sư 4 năm môn tiếng anh, toán cấp 1,2', 1, NULL),
+    (4, 1, '2003-11-18', N'Hà Nội', N'Đại học', 4, N'Kinh nghiệm gia sư 4 năm môn tiếng anh cấp 1,2,3', 1, NULL),
+    (5, 0, '2002-07-28', N'Hà Nội', N'Đại học', 3, N'Kinh nghiệm gia sư 3 năm môn toán cấp 2', 1, NULL),
+    (6, 1, '2003-06-19', N'Hà Nội', N'Đại học', 3, N'Kinh nghiệm gia sư 3 năm môn toán, anh lớp 8,9', 1, NULL),
+    (7, 1, '2003-08-10', N'Hà Nội', N'Đại học', 2, N'Kinh nghiệm gia sư 2 năm môn tiếng anh cấp 2 và 3; 2 năm môn toán cấp 2', 1, NULL),
+    (8, 1, '2003-06-22', N'Hà Nội', N'Đại học', 3, N'Kinh nghiệm gia sư 3 năm môn anh cấp 1 và cấp 2', 1, NULL),
+    (9, 0, '2003-07-08', N'Hà Nội', N'Đại học', 3, N'Kinh nghiệm gia sư 3 năm môn toán cấp 2', 1, NULL),
+    (10, 0, '2003-07-29', N'Hà Nội', N'Đại học', 3, N'Gia sư 3 năm cho học sinh THCS, THPT (6,7,8,9,10)', 1, NULL),
+    (11, 0, '2003-09-08', N'Hà Nội', N'Đại học', 1, N'Kinh nghiệm gia sư 1 năm môn tiếng anh cấp 1', 1, NULL),
+    (12, 0, '2003-09-25', N'Hà Nội', N'Đại học', 3, N'Kinh nghiệm gia sư 3 năm môn tiếng anh', 1, NULL),
+    (13, 0, '2003-03-06', N'Hà Nội', N'Đại học', 2, N'Kinh nghiệm gia sư 2 năm môn toán cấp 3', 1, NULL),
+    (14, 1, '2003-04-29', N'Hà Nội', N'Đại học', 1, N'Kinh nghiệm gia sư 01 năm, dạy môn toán cấp 3', 1, NULL),
+    (15, 1, '2003-10-23', N'Hà Nội', N'Đại học', 1, N'Kinh nghiệm gia sư 01 năm, dạy toán cấp 1,2,3', 1, NULL),
+    (16, 1, '2003-03-27', N'Hà Nội', N'Đại học', 1, N'Kinh nghiệm gia sư 1 năm môn tiếng anh cấp 2,3', 1, NULL),
+    (17, 1, '2003-09-08', N'Hà Nội', N'Đại học', 1, N'Kinh nghiệm giảng dạy 01 năm môn tiếng Trung sơ cấp', 1, NULL);
+-- Thêm dữ liệu vào bảng Tutors
+UPDATE Tutors
+SET money_per_slot = CASE ID
+                         WHEN 1 THEN 200000 -- Trần Thiên Hương
+                         WHEN 2 THEN 250000 -- Bùi Thị Linh Đan
+                         WHEN 3 THEN 200000 -- Nguyễn Thị Mai
+                         WHEN 4 THEN 200000 -- Nguyễn Quế Chi
+                         WHEN 5 THEN 200000 -- Hà Minh Quân
+                         WHEN 6 THEN 120000 -- Vương Thị Thanh Thủy (tính theo giờ)
+                         WHEN 7 THEN 200000 -- Nguyễn Thị Quyết Tâm
+                         WHEN 8 THEN 170000 -- Trương Ngọc Ánh
+                         WHEN 9 THEN 200000 -- Đinh Ngọc Khôi
+                         WHEN 10 THEN 45000  -- Bùi Quang Huy (45k/buổi/lớp tối đa 15 người)
+                         WHEN 11 THEN 160000 -- Đinh Tiến Đạt
+                         WHEN 12 THEN 200000 -- Nguyễn Thái Đức
+                         WHEN 13 THEN 150000 -- Đỗ Tiến Dũng
+                         WHEN 14 THEN 200000 -- Trần Thị Thanh Trang
+                         WHEN 15 THEN 150000 -- Phạm Ngọc Hà
+                         WHEN 16 THEN 200000 -- Dương Quỳnh Trang
+                         WHEN 17 THEN 100000 -- Đặng Quỳnh Anh
+    END
+WHERE ID IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
