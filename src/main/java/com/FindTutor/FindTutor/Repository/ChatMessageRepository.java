c02f7a8c19bb6b978fb7b9a4ca84aa507f7da4e8
HungNgo03 <hungnnhe173202@fpt.edu.vn>
2025-03-10 17:11:42 +0700

    update sql chat

--- src/main/java/com/FindTutor/FindTutor/Repository/ChatMessageRepository.java
+++ src/main/java/com/FindTutor/FindTutor/Repository/ChatMessageRepository.java
@@ -11,7 +11,10 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> 
     @Query("SELECT m FROM ChatMessage m WHERE (m.sender = :sender AND m.receiver = :receiver) OR (m.sender = :receiver AND m.receiver = :sender) ORDER BY m.timestamp ASC")
     List<ChatMessage> findChatHistory(@Param("sender") String sender, @Param("receiver") String receiver);
 
-    @Query("SELECT DISTINCT CASE WHEN m.sender = 'admin' THEN m.receiver ELSE m.sender END " +
-            "FROM ChatMessage m WHERE m.sender = 'admin' OR m.receiver = 'admin'")
+    @Query("SELECT DISTINCT COALESCE(CASE WHEN m.sender = 'admin' THEN m.receiver ELSE m.sender END, '') " +
+            "FROM ChatMessage m " +
+            "WHERE (m.sender = 'admin' OR m.receiver = 'admin') " +
+            "AND m.receiver IS NOT NULL AND m.sender IS NOT NULL")
     List<String> findChattedUsersForAdmin();
+
 }
